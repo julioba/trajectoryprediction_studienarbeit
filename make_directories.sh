b60454e22469e0a71bf9ed729b3ec540b89c57d8
julioba <49531680+julioba@users.noreply.github.com>
2019-10-02 21:05:50 +0200

    Update make_directories.sh

--- make_directories.sh
+++ make_directories.sh
@@ -3,106 +3,368 @@
 
 mkdir -p log
 cd log
+
 mkdir -p SOCIALLSTM
-cd SOCIALLSTM
-mkdir -p GRU
-mkdir -p LSTM
-cd ..
 mkdir -p OBSTACLELSTM
-cd OBSTACLELSTM
-mkdir -p GRU
-mkdir -p LSTM
-cd ..
 mkdir -p VANILLALSTM
-cd VANILLALSTM
-mkdir -p GRU
+cd SOCIALLSTM
 mkdir -p LSTM
 cd ..
+
+cd OBSTACLELSTM
+mkdir -p LSTM
 cd ..
+
+cd VANILLALSTM
+mkdir -p LSTM
+cd ..
+
+cd ..
+
 
 
 mkdir -p model
+
 cd model
+
 mkdir -p SOCIALLSTM
-cd SOCIALLSTM
-mkdir -p GRU
-mkdir -p LSTM
-cd ..
 mkdir -p OBSTACLELSTM
-cd OBSTACLELSTM
-mkdir -p GRU
-mkdir -p LSTM
-cd ..
 mkdir -p VANILLALSTM
-cd VANILLALSTM
-mkdir -p GRU
+
+cd SOCIALLSTM
 mkdir -p LSTM
+cd LSTM
+cd > config.pkl
+
+cd ..
+cd .. 
+
+cd OBSTACLELSTM
+mkdir -p LSTM
+cd LSTM
+cd > config.pkl
+
 cd ..
 cd ..
+
+cd VANILLALSTM
+mkdir -p LSTM
+cd LSTM
+cd > config.pkl
+cd ..
+cd ..
+
+cd ..
+
 
 mkdir -p result
 cd result
+
 mkdir -p SOCIALLSTM
-cd SOCIALLSTM
-mkdir -p GRU
-mkdir -p LSTM
-cd ..
 mkdir -p OBSTACLELSTM
-cd OBSTACLELSTM
-mkdir -p GRU
-mkdir -p LSTM
-cd ..
 mkdir -p VANILLALSTM
-cd VANILLALSTM
-mkdir -p GRU
+
+cd SOCIALLSTM
+
 mkdir -p LSTM
+cd LSTM
+mkdir -p test
+cd test
+mkdir -p simulation
+mkdir -p stpet_residence
+mkdir -p tudresden
+cd simulation
+cd > simulation_test.txt
+cd ..
+
+cd stpet_residence
+cd > residence_simulation.txt
+cd ..
+cd tudresden
+cd > residence_real.txt
 cd ..
 cd ..
+cd ..
+cd ..
+
+cd OBSTACLELSTM
+
+mkdir -p LSTM
+cd LSTM
+mkdir -p test
+cd test
+mkdir -p simulation
+mkdir -p stpet_residence
+mkdir -p tudresden
+cd simulation
+cd > simulation_test.txt
+cd ..
+cd stpet_residence
+cd > residence_simulation.txt
+cd ..
+cd tudresden
+cd > residence_real.txt
+cd ..
+cd .. 
+cd ..
+cd ..
+
+cd VANILLALSTM
+
+mkdir -p LSTM
+cd LSTM
+mkdir -p test
+cd test
+mkdir -p simulation
+mkdir -p stpet_residence
+mkdir -p tudresden
+cd simulation
+cd > simulation_test.txt
+cd ..
+cd stpet_residence
+cd > residence_simulation.txt
+cd ..
+cd tudresden
+cd > residence_real.txt
+cd ..
+cd ..
+cd ..
+cd ..
+
+cd .. 
+
 
 mkdir -p plot
 cd plot
+mkdir -p Superposition
 mkdir -p SOCIALLSTM
-cd SOCIALLSTM
-mkdir -p GRU
-cd GRU
-mkdir -p plots
-mkdir -p videos
-cd ..
-mkdir -p LSTM
-cd LSTM
-mkdir -p plots
-mkdir -p videos
-cd ..
-cd ..
 mkdir -p OBSTACLELSTM
-cd OBSTACLELSTM
-mkdir -p GRU
-cd GRU
-mkdir -p plots
-mkdir -p videos
-cd ..
-mkdir -p LSTM
-cd LSTM
-mkdir -p plots
-mkdir -p videos
-cd ..
-cd ..
 mkdir -p VANILLALSTM
-cd VANILLALSTM
-mkdir -p GRU
-cd GRU
-mkdir -p plots
-mkdir -p videos
-cd ..
+
+cd SOCIALLSTM
 mkdir -p LSTM
 cd LSTM
 mkdir -p plots
+mkdir -p videos
+mkdir -p test
+mkdir -p validation
+cd test
+mkdir -p simulation
+mkdir -p stpet_residence
+mkdir -p tudresden
+cd simulation
+cd > simulation_test.pkl
+cd ..
+cd stpet_residence
+cd > residence_simulation.pkl
+cd ..
+cd tudresden
+cd > residence_real.pkl
+cd ..
+
+cd ..
+
+cd validation
+
+mkdir -p simulation
+mkdir -p stpet_residence
+mkdir -p tudresden
+cd simulation
+
+cd > simulation_validation.pkl
+cd ..
+cd stpet_residence
+
+cd > stpet_residence_validation.pkl
+cd ..
+
+cd tudresden
+
+cd > residence_real.pkl
+cd ..
+
+cd ..
+mkdir -p videos
+cd ..
+cd ..
+
+cd OBSTACLELSTM
+
+mkdir -p LSTM
+cd LSTM
+
+mkdir -p plots
+mkdir -p videos
+mkdir -p test
+mkdir -p validation
+cd test
+mkdir -p simulation
+mkdir -p stpet_residence
+mkdir -p tudresden
+cd simulation
+
+cd > simulation_test.pkl
+
+cd ..
+
+cd stpet_residence
+cd > residence_simulation.pkl
+cd ..
+
+cd tudresden
+
+cd > residence_real.pkl
+cd ..
+
+cd ..
+
+cd validation
+
+mkdir -p simulation
+mkdir -p stpet_residence
+mkdir -p tudresden
+cd simulation
+
+cd > simulation_validation.pkl
+cd ..
+
+cd stpet_residence
+
+cd > stpet_residence_validation.pkl
+cd ..
+
+cd tudresden
+
+cd > residence_real.pkl
+cd ..
+
+cd ..
+mkdir -p videos
+cd ..
+cd ..
+
+
+
+cd VANILLALSTM
+
+mkdir -p LSTM
+cd LSTM
+mkdir -p plots
+mkdir -p videos
+mkdir -p test
+mkdir -p validation
+cd test
+
+mkdir -p simulation
+mkdir -p stpet_residence
+mkdir -p tudresden
+cd simulation
+
+cd > simulation_test.pkl
+cd ..
+
+cd stpet_residence
+
+cd > residence_simulation.pkl
+cd ..
+
+cd tudresden
+
+cd > residence_real.pkl
+cd ..
+cd ..
+
+cd validation
+
+mkdir -p simulation
+mkdir -p stpet_residence
+mkdir -p tudresden
+cd simulation
+
+cd > simulation_validation.pkl
+
+cd ..
+
+cd stpet_residence
+
+cd > stpet_residence_validation.pkl
+cd ..
+
+cd tudresden
+
+cd > residence_real.pkl
+cd ..
+
+cd ..
 mkdir -p videos
 cd ..
 cd ..
 cd ..
 
+mkdir -p data
 cd data
 mkdir -p validation
+mkdir -p test
+mkdir -p train
+cd validation
+mkdir -p simulation
+mkdir -p stpet_residence
+mkdir -p tudresden
+
+cd simulation
+cd > simulation_test.txt
+cd ..
+
+cd stpet_residence
+cd > residence_simulation.txt
+cd ..
+
+cd tudresden 
+cd > residence_real.txt
+cd ..
+
+cd ..
+
+cd train
+mkdir -p simulation
+mkdir -p stpet_residence
+mkdir -p tudresden
+
+cd simulation
+cd > simulation_test.txt
+cd ..
+
+cd stpet_residence
+cd > residence_simulation.txt
+cd ..
+
+cd tudresden 
+cd > residence_real.txt
+cd ..
+
+cd ..
+
+cd validation
+mkdir -p simulation
+mkdir -p stpet_residence
+mkdir -p tudresden
+
+cd simulation
+cd > simulation_test.txt
+cd ..
+
+cd stpet_residence
+cd > residence_simulation.txt
+cd ..
+
+cd tudresden 
+cd > residence_real.txt
+cd ..
+
+cd ..
+
+
+
+cd ..
 cd ..
 
